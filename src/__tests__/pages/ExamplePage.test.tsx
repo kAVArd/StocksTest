66cbaf46f7c06e2906eba7509ee2813b350347a0
Ilya Savinov <savinovilja1999@gmail.com>
2024-01-07 03:13:33 +0700

    add socket emit test

--- src/__tests__/pages/ExamplePage.test.tsx
+++ src/__tests__/pages/ExamplePage.test.tsx
@@ -6,6 +6,7 @@ import userEvent from '@testing-library/user-event'
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import ExamplePage from '../../pages/ExamplePage/ExamplePage';
+import io from 'socket.io-client';
 
 let mockEmitter = jest.fn();
 jest.mock('socket.io-client', () => {
@@ -19,14 +20,16 @@ const mockStore = configureStore([]);
 
 describe('ExamplePage', () => {
   let store: any;
+  let socket: any;
 
   beforeEach(() => {
     store = mockStore({ ticker: {
       data: { AAPL: [] }
     } });
+
   });
 
-  it('should render without crashing', () => {
+  it('socket emit test', () => {
     render(
       <Provider store={store}>
         <ExamplePage />
@@ -35,6 +38,8 @@ describe('ExamplePage', () => {
   });
 
   it('should handle input change', async () => {
+    const mockSocket = io('http://localhost:5000');
+
     const { getByPlaceholderText, getByText, container } = render(
       <Provider store={store}>
         <ExamplePage />
@@ -46,5 +51,9 @@ describe('ExamplePage', () => {
     userEvent.type(input, "AAPL");
 
     expect(input).toHaveValue('AAPL');
+
+    userEvent.click(getByText('Get Data'));
+
+    expect(mockSocket.emit).toHaveBeenCalledWith('ticker', 'AAPL');
   });
 });
